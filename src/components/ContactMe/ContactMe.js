1a5cd67fecf1fc80868ceedde7e31edbad940bbc
Alec Bana <anbana994@gmail.com>
2024-03-06 23:29:56 -0500

    added new styles to the form

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ContactMe.scss';
 
 function Contact() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+
   return (
     <section className="contact">
-      <h2 className="contact__title">Contact Me</h2>
-      <form className="contact__form">
-        <input type="text" className="contact__input" placeholder="Your Name" />
-        <input type="email" className="contact__input" placeholder="Your Email" />
-        <textarea className="contact__textarea" placeholder="Your Message"></textarea>
-        <button type="submit" className="contact__button">Send Message</button>
-      </form>
+      <button onClick={toggleModal} className="contact__title-button">Contact Me</button>
+      <a href="/path/to/your/resume.pdf" download className="contact__resume-download">Download Resume</a>
+
+      {isModalOpen && (
+        <div className="contact__modal">
+          <form className="contact__form">
+            <input type="text" className="contact__input" placeholder="Your Name" />
+            <input type="email" className="contact__input" placeholder="Your Email" />
+            <textarea className="contact__textarea" placeholder="Your Message"></textarea>
+            <button type="submit" className="contact__button">Send Message</button>
+          </form>
+        </div>
+      )}
     </section>
   );
 }
