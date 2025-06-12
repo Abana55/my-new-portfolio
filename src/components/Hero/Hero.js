63656c433292bc018cd0a6595085ccc70b4383d9
Alec Bana <anbana994@gmail.com>
2025-06-11 20:23:05 -0400

    removed the 3js component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,34 +2,25 @@ import React from "react";
 import "./Hero.scss";
 import Skills from "../Skills/Skills";
 import { Link } from "react-router-dom";
-import { Canvas } from '@react-three/fiber';
-import SpinningCube from '../SpinningCube/SpinningCube'; 
-import { Environment } from '@react-three/drei';
-
 
 function Hero() {
   return (
-    <div className="hero">
-        <SpinningCube />
+    <section className="hero">
+      {/* Foreground content only */}
       <div className="hero__content">
         <h1 className="hero__title">Welcome to My Portfolio</h1>
         <p className="hero__description">
-          I'm a software engineer based in Miami, FL. Passionate about building
+          I’m a software engineer based in Miami, FL. Passionate about building
           efficient and scalable web applications, I specialize in full-stack
-          development with a focus on modern technologies like React, Node.js,
-          and Express as well as databases like MySQL.
+          development with modern technologies like React, Node.js, and MySQL.
         </p>
         <div className="hero__buttons">
-          <Link to="/about" className="hero__button">
-            About Me
-          </Link>
-          <Link to="/projects" className="hero__button">
-            My Projects
-          </Link>
+          <Link to="/about" className="hero__button">About Me</Link>
+          <Link to="/projects" className="hero__button">My Projects</Link>
         </div>
+        <Skills />
       </div>
-      <Skills />
-    </div>
+    </section>
   );
 }
 
--- src/components/SpinningCube/SpinningCube.js
+++ src/components/SpinningCube/SpinningCube.js
@@ -1,23 +1,5 @@
-import React, { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
-import { MeshBasicMaterial, SphereBufferGeometry } from 'three';
-
-const SpinningSphere = () => {
-  const meshRef = useRef();
-
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.y += 0.01;
-    }
-  });
-
-  return (
-    <mesh ref={meshRef}>
-      <sphereBufferGeometry args={[1, 32, 32]} />
-      <meshBasicMaterial color="red" />
-    </mesh>
-  );
-};
-
-export default SpinningSphere;
+// src/components/SpinningSphere/SpinningSphere.js
+import React from "react";
+export default function SpinningSphere() {
+  return null;
+}
